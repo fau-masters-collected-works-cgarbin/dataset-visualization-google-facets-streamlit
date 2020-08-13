be79c7e78f32309bb6650a2d4ef01e8d362c21a7
Christian Garbin <garbin.christian@gmail.com>
2020-08-13 07:36:20 -0400

    Add age groups

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 '''Preprocess the dataset
 
 - Separate the diseases into indicator (true/false) columns
-- Bin the ages
+- Bin the ages into MeSH age groups
 - Mark images as either training or test
 
 See more details about the dataset in the data folder.
@@ -11,6 +11,8 @@ import pandas as pd
 _DATASET = 'data/Data_Entry_2017_v2020.csv'
 _TRAIN_VAL_LIST = 'data/train_val_list.txt'
 _TEST_LIST = 'data/test_list.txt'
+
+_AGE_GROUP = 'Age Group'
 
 
 def _verify_dataset(ds: pd.DataFrame):
@@ -30,16 +32,23 @@ def _verify_dataset(ds: pd.DataFrame):
     # Number of rows must match the original dataset
     assert ds.shape[0] == 112120
 
-    # Columns must match the original 11, plus:
-    #    - 15 for each disease (14) + 'no finding'
-    assert ds.shape[1] == 11 + 15
+    # Number of columns must match the original 11, plus:
+    #    - 15 for disease indicators (14 + 'no finding')
+    #    - 1 for the age group
+    assert ds.shape[1] == 11 + 15 + 1
+
+    # Check a few disease indicators and age group
+    assert _AGE_GROUP in ds.columns
+    assert 'Mass' in ds.columns
+    assert 'No Finding' in ds.columns
 
 
 def get_dataset() -> pd.DataFrame:
     '''Reads the raw files that comprise the dataset and creates a preprocessed dataset.
 
     Returns:
-        The preprocessed dataset.
+        The preprocessed dataset: diseases are split into indicator (true/false) columns and ages
+        are binned.
     '''
 
     ds = pd.read_csv(_DATASET)
@@ -47,8 +56,17 @@ def get_dataset() -> pd.DataFrame:
     # Split diseases into separate indicator columns
     indicators = ds['Finding Labels'].str.get_dummies('|')
     ds = pd.concat([ds, indicators], axis='columns')
+
+    # Bin the ages according to MeSH age groups
+    # Best reference I found for MeSH groups: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1794003/
+    # We have only complete years, so we can't use 'newborn'
+    bins = [0, 2, 6, 13, 19, 45, 65, 80, 120]
+    labels = ['Infant', 'Preschool', 'Child', 'Adolescent',
+              'Adult', 'Middle age', 'Aged', 'Aged 80']
+    ds[_AGE_GROUP] = pd.cut(ds['Patient Age'], bins=bins, labels=labels, right=False)
+
     print(ds)
-    print(indicators)
+    # print(indicators)
     _verify_dataset(ds)
     return ds
 

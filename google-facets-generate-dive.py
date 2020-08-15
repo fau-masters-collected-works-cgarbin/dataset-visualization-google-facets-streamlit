33e5a9b63f45c76d06281531d661f6eb27dc49d5
Christian Garbin <garbin.christian@gmail.com>
2020-08-15 08:34:26 -0400

    Add Google Facets Dive generator

---
+++ google-facets-generate-dive.py
@@ -0,0 +1,56 @@
+'''Generates the HTML page for the Google Facets Dive for ChestX-ray14.
+
+See the "Dive" section in https://pair-code.github.io/facets/.
+
+Based on https://colab.research.google.com/github/PAIR-code/facets/blob/master/colab_facets.ipynb#scrollTo=b7zs2p2_goJa, # noqa
+via https://github.com/PAIR-code/facets#setup.
+
+Note that the dataset is embedded in the HTML in JSON format. An improvement would be to embbed it
+in CSV format and convert to JSON when the page is loaded.
+'''
+
+import dataset
+
+
+def get_sprite_size(dataset_size):
+    '''Sets the sprite_size based on the number of records in dataset.
+
+    Larger datasets can crash the browser if the size is too large (>50000)
+
+    Args:
+        The size (number of entries) in the dataset.
+
+    Returns:
+        The sprite size, adjusted for the dataset size.
+    '''
+    if dataset_size > 100000:
+        return 16
+    else:
+        return 32 if dataset_size > 50000 else 64
+
+
+if __name__ == "__main__":
+    ds = dataset.get_dataset()
+    ds = dataset.reduce_size(ds, remove_indicators=False)
+
+    jsonstr = ds.to_json(orient='records')
+
+    sprite_size = get_sprite_size(len(ds.index))
+
+    HTML_TEMPLATE = (
+        '<script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-lite.js"></script>'  # noqa
+        '<link rel="import" href="https://raw.githubusercontent.com/PAIR-code/facets/1.0.0/facets-dist/facets-jupyter.html">'  # noqa
+        '<facets-dive sprite-image-width="{sprite_size}" sprite-image-height="{sprite_size}" id="elem"></facets-dive>'  # noqa
+        '<script>'
+        '   var data = {jsonstr};'
+        '   document.querySelector("#elem").data = data;'
+        '</script>')
+
+    html = HTML_TEMPLATE.format(jsonstr=jsonstr, sprite_size=sprite_size)
+
+    FILE = 'google-facets-dive.html'
+    with open(FILE, 'w') as file:
+        print(html, file=file)
+
+    print('Open {} in a Chrome browser (must be Chrome)'.format(FILE))
+    print('It will take a few seconds to load')
